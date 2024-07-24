424240b3b5a0141d1e7d0c8ae9c8ed67a50d115d
Parth Agarwal <agarwal.parth.101@gmail.com>
2024-07-24 22:58:32 +0530

    linting change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='fpdb',
-    packages=['fpdb',],
+    packages=['fpdb'],
     license='MIT',
     install_requires=[
         "pywin32; os_name=='nt'",
